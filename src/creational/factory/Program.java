e6126708bc5796fdf8e3c75836dd4f784df903e9
zotov88 <zotov_l88@mail.ru>
2023-08-23 15:15:40 +0300

    factory

--- src/creational/factory/Program.java
+++ src/creational/factory/Program.java
@@ -2,6 +2,18 @@ package creational.factory;
 
 public class Program {
     public static void main(String[] args) {
+        DeveloperFactory developerFactory = getDeveloperFactoryBySpeciality("java");
+        Developer developer = developerFactory.createDeveloper();
+        developer.writeCode();
+    }
 
+    static DeveloperFactory getDeveloperFactoryBySpeciality(final String speciality) {
+        if (speciality.equalsIgnoreCase("java")) {
+            return new JavaDeveloperFactory();
+        } else if (speciality.equalsIgnoreCase("C++")) {
+            return new CppDeveloperFactory();
+        } else {
+            throw new RuntimeException(speciality + " is unknown");
+        }
     }
 }
